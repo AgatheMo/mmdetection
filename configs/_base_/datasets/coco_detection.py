404e10809532f61500569fe9004daa57a0d565a0
AgatheMo <60713294+AgatheMo@users.noreply.github.com>
2021-01-27 09:34:08 +0100

    Update coco_detection.py

--- configs/_base_/datasets/coco_detection.py
+++ configs/_base_/datasets/coco_detection.py
@@ -1,5 +1,5 @@
 dataset_type = 'CocoDataset'
-data_root = 'datasets/coco/'
+data_root = 'datasetsSJ/coco/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
@@ -32,17 +32,17 @@ data = dict(
     workers_per_gpu=2,
     train=dict(
         type=dataset_type,
-        ann_file=data_root + 'annotations/train_segmentation.json',
-        img_prefix=data_root + 'images_train/',
+        ann_file=data_root + 'annotations/train_segmentation_SJ.json',
+        img_prefix=data_root + 'images_SJ/',
         pipeline=train_pipeline),
     val=dict(
         type=dataset_type,
-        ann_file=data_root + 'annotations/test_segmentation.json',
-        img_prefix=data_root + 'images_test_SJ/',
+        ann_file=data_root + 'annotations/test_segmentation_SJ.json',
+        img_prefix=data_root + 'images_SJ/',
         pipeline=test_pipeline),
     test=dict(
         type=dataset_type,
-        ann_file=data_root ,
-        img_prefix=data_root + 'images_test_SJ/',
+        ann_file=data_root + 'annotations/test_segmentation_SJ.json',
+        img_prefix=data_root + 'images_SJ/',
         pipeline=test_pipeline))
 evaluation = dict(interval=1, metric='bbox')
