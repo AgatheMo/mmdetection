f75e323d3d410c0204d09a1b1dfeb51cd81af14e
AgatheMo <60713294+AgatheMo@users.noreply.github.com>
2021-02-11 22:13:59 +0100

    Update yolact_r50_1x8_coco.py

--- configs/yolact/yolact_r50_1x8_coco.py
+++ configs/yolact/yolact_r50_1x8_coco.py
@@ -146,7 +146,7 @@ data = dict(
         img_prefix=data_root + 'images_test/',
         pipeline=test_pipeline))
 # optimizer
-optimizer = dict(type='SGD', lr=0.0025, momentum=0.9, weight_decay=5e-4)
+optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=5e-4)
 optimizer_config = dict()
 # learning policy
 lr_config = dict(
