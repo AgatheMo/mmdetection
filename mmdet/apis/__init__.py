767a09ab45c35b374aa3839a7087d0da09a7a482
AgatheMo <60713294+AgatheMo@users.noreply.github.com>
2021-02-14 15:04:05 -0500

    Update __init__.py

--- mmdet/apis/__init__.py
+++ mmdet/apis/__init__.py
@@ -1,6 +1,6 @@
 from .inference import (async_inference_detector, inference_detector,
                         init_detector, show_result_pyplot)
-from .test import multi_gpu_test, single_gpu_test
+from .test import multi_gpu_test, single_gpu_test, single_gpu_test_pp
 from .train import get_root_logger, set_random_seed, train_detector
 
 __all__ = [
