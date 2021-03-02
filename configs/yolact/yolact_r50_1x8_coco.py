9e1e363b470cb538f8ffd738b219a40e6f3259e7
AgatheMo <60713294+AgatheMo@users.noreply.github.com>
2021-03-01 19:13:12 -0500

    Update yolact_r50_1x8_coco.py

--- configs/yolact/yolact_r50_1x8_coco.py
+++ configs/yolact/yolact_r50_1x8_coco.py
@@ -146,14 +146,14 @@ data = dict(
         img_prefix=data_root + 'images_SJ/',
         pipeline=test_pipeline))
 # optimizer
-optimizer = dict(type='SGD', lr=1e-3, momentum=0.9, weight_decay=5e-4)
+optimizer = dict(type='SGD', lr=0.0005, momentum=0.9, weight_decay=5e-4)
 optimizer_config = dict()
 # learning policy
 lr_config = dict(
     policy='step',
     warmup='linear',
     warmup_iters=500,
-    warmup_ratio=0.1,
+    warmup_ratio=0.001,
     step=[20, 42, 49, 52])
 total_epochs = 55
 cudnn_benchmark = True
