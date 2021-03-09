f56a3dbea393f4ffd7dbd487044df185a2839239
AgatheMo <60713294+AgatheMo@users.noreply.github.com>
2021-03-09 11:20:48 -0500

    Update yolact_r50_1x8_coco.py

--- configs/yolact/yolact_r50_1x8_coco.py
+++ configs/yolact/yolact_r50_1x8_coco.py
@@ -155,6 +155,6 @@ lr_config = dict(
     warmup_iters=500,
     warmup_ratio=0.001,
     step=[20, 42, 49, 52])
-total_epochs = 75
+runner = dict(type='EpochBasedRunner', max_epochs=55)
 cudnn_benchmark = True
 evaluation = dict(interval=1,metric=['bbox', 'segm'])
